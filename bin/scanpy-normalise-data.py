ebf21c07441af71fa4710ddbf4440519acb6291f
nh3 <nh3@users.noreply.github.com>
2018-10-08 21:50:00 +0100

    Change lettering of command line option

--- bin/scanpy-normalise-data.py
+++ bin/scanpy-normalise-data.py
@@ -13,7 +13,7 @@ def main(args):
 
     adata = read_input_object(args.input_object_file, args.input_format)
 
-    sc.pp.normalize_per_cell(adata, counts_per_cell_after=args.counts_per_cell)
+    sc.pp.normalize_per_cell(adata, counts_per_cell_after=args.scale_factor)
 
     write_output_object(adata, args.output_object_file, args.output_format)
 
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     argparser = ScanpyArgParser('Normalise per-cell quantification data')
     argparser.add_input_object()
     argparser.add_output_object()
-    argparser.parser.add_argument('-c', '--counts-per-cell',
+    argparser.parser.add_argument('-s', '--scale-factor',
                                   type=float,
                                   default=1e4,
                                   help='Aimed counts per cell after normalisation, default: 1e4')
