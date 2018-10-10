509de6c392e4ffae9543d6647819abd2e2d677a1
nh3 <nh3@users.noreply.github.com>
2018-10-10 12:45:45 +0100

    Appropriate changes to use updated (0b9770) scanpy_wrapper_utils.py
    Minor style changes to make pylint happier.
    Fixed a typo in read_input_object()

--- bin/scanpy-filter-cells.py
+++ bin/scanpy-filter-cells.py
@@ -1,62 +1,49 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-import signal
 import logging
-import pandas as pd
 import scanpy.api as sc
 from scanpy_wrapper_utils import ScanpyArgParser, read_input_object, write_output_object
-signal.signal(signal.SIGPIPE, signal.SIG_DFL)
-
-
-def read_subset_items(input_string):
-    if ',' in input_string:
-        list_parser = comma_separated_list('cells-use', str)
-        return list_parser(input_string)
-    else:
-        return list(pd.read_table(input_string, header=None).iloc[:, 0].values)
 
 
 def main(args):
     logging.debug(args)
 
-    adata = read_input_object(args.input_object_file, args.output_object_file)
+    adata = read_input_object(args.input_object_file, args.input_format)
 
-    if args.cells_use is not None:
-        cells_to_use = read_subset_items(args.cells_use)
-        adata = adata[:, adata.obs.isin(cells_to_use)]
+    if args.subset_list:
+        adata = adata[:, adata.obs.isin(args.subset_list)]
 
     inf, neg_inf = float('Inf'), float('-Inf')
-    for name, h, l in zip(args.subset_names, args.high_thresholds, args.low_thresholds):
+    for name, high, low in zip(args.parameter_names, args.high_thresholds, args.low_thresholds):
         if name == 'n_genes':
-            if l > neg_inf:
-                sc.pp.filter_cells(adata, min_genes=l)
-            if h < inf:
-                sc.pp.filter_cells(adata, max_genes=h)
+            if low > neg_inf:
+                sc.pp.filter_cells(adata, min_genes=low)
+            if high < neg_inf:
+                sc.pp.filter_cells(adata, max_genes=high)
         elif name == 'n_counts':
-            if l > neg_inf:
-                sc.pp.filter_cells(adata, min_counts=l)
-            if h < inf:
-                sc.pp.filter_genes(adata, max_counts=h)
+            if low > neg_inf:
+                sc.pp.filter_cells(adata, min_counts=low)
+            if high < neg_inf:
+                sc.pp.filter_cells(adata, max_counts=high)
         elif name not in adata.obs.columns:
-            logging.warning('subset-name "{}" not present in data, omitted'.format(name))
+            msg = 'parameter-name "{}" not present in data, omitted'.format(name)
+            logging.warning(msg)
         else:
-            adata = adata[(adata.obs[name] < h) & (adata.obs[name] > l), :]
+            adata = adata[(adata.obs[name] < high) & (adata.obs[name] > low), :]
 
     write_output_object(adata, args.output_object_file, args.output_format)
 
     logging.info('Done')
     return 0
- 
+
 
 if __name__ == "__main__":
     argparser = ScanpyArgParser('Filter genes for ScanPy')
     argparser.add_input_object()
     argparser.add_output_object()
     argparser.add_subset_parameters()
-    argparser.parser.add_argument('-c', '--cells-use',
-                                  help='Comma-separated list of cell names to use as a subset. '
-                                       'Alternatively, text file with one cell per line.')
+    argparser.add_subset_list()
     args = argparser.get_args()
 
     main(args)
