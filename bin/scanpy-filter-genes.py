50093ed1047893fd1094749eff0ac598ddb0f643
nh3 <nh3@users.noreply.github.com>
2018-10-11 11:48:38 +0100

    Feature/filter genes (#1)
    * First implementation of scanpy-filter-genes
    
    * code restructure
    
    * code restructure
    
    * Add scanpy_wrapper_utils.py from feature/read10x
    
    * Improved and unified interface for argument checking
    
    * Major code restructure
    
    Use scanpy_wrapper_utils.py for argument parsing.
    Call sc.pp.filter_genes() to calculate and filter on n_counts, n_cells.
    
    * Update scanpy_wrapper_utils.py from feature/read10x
    
    * Code style improvements to make pylint happier
    
    * Code style improvement to make pylint happier
    
    * Update scanpy_wrapper_utils.py from feature/read10x
    
    * Allow reading subset as either a filename or a comma separated list
    
    * Add parser for list-or-file type of args, and some arg naming change
    
    * Use updated scanpy_wrapper_utils.py (3c4c750) to handle subset list
    
    * Add plot-related functions and improve arg checking interface
    
    1. Add Scanpy.ArgParser.add_argument() and hide its parser attribute,
       requires appropriate change of scripts accessing it directly.
    2. Add ScanpyArgParser.add_output_plot()
    3. Add ScanpyArgParser._check_filename_extension()
    4. Redisgn arg checking event as 3-item list: [handler, args, kwargs]
       to allow passing key word argument
    
    * Minor style changes, slight change in command description

---
+++ bin/scanpy-filter-genes.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import logging
+import scanpy.api as sc
+from scanpy_wrapper_utils import ScanpyArgParser, read_input_object, write_output_object
+
+
+def main(args):
+    logging.debug(args)
+
+    adata = read_input_object(args.input_object_file, args.input_format)
+
+    if args.subset_list:
+        adata = adata[:, adata.var_names.isin(args.subset_list)]
+
+    inf, neg_inf = float('Inf'), float('-Inf')
+    for name, high, low in zip(args.parameter_names, args.high_thresholds, args.low_thresholds):
+        if name == 'n_cells':
+            if low > neg_inf:
+                sc.pp.filter_genes(adata, min_cells=low)
+            if high < inf:
+                sc.pp.filter_genes(adata, max_cells=high)
+        elif name == 'n_counts':
+            if low > neg_inf:
+                sc.pp.filter_genes(adata, min_counts=low)
+            if high < inf:
+                sc.pp.filter_genes(adata, max_counts=high)
+        elif name not in adata.var.columns:
+            msg = 'parameter-name "{}" not present in data, omitted'.format(name)
+            logging.warning(msg)
+        else:
+            adata = adata[(adata.var[name] < high) & (adata.var[name] > low), :]
+
+    write_output_object(adata, args.output_object_file, args.output_format)
+
+    logging.info('Done')
+    return 0
+
+
+if __name__ == '__main__':
+    argparser = ScanpyArgParser('Filter genes by properties and/or simple stats')
+    argparser.add_input_object()
+    argparser.add_output_object()
+    argparser.add_subset_parameters()
+    argparser.add_subset_list()
+    args = argparser.get_args()
+
+    main(args)
--- bin/scanpy_wrapper_utils.py
+++ bin/scanpy_wrapper_utils.py
@@ -7,8 +7,11 @@
 """
 
 import sys
+import os.path
+import shutil
 import logging
 import argparse
+import pandas as pd
 import scanpy.api as sc
 
 
@@ -50,62 +53,115 @@ class ScanpyArgParser():
         + description : str
         A string passed to argparse.ArgumentParser() summarising the usage of the script.
 
-    * Attributes
-        + parser : argparse.ArgumentParser
-        The actual argument parser that parses the command line input
+    * Methods
+        + get_args()
+        + add_argument(*args, **kwargs)
+        + add_input_object()
+        + add_output_object()
+        + add_output_plot()
+        + add_subset_parameters(params=None)
+        + add_subset_list(dtype=str)
     """
     def __init__(self, description=None):
-        self.parser = argparse.ArgumentParser(description=description)
-        self.parser.add_argument('--debug', action='store_true',
-                                 help='Print debug information.')
-        self._events = [{'handler':self._set_logging_level,
-                         'argv':[]}]
+        self._parser = argparse.ArgumentParser(description=description)
+        self._parser.add_argument('--debug', action='store_true',
+                                  help='Print debug information.')
+        self._events = [[self._set_logging_level, [], {}]]
         self._args = None
+
+
+    def get_args(self):
+        """Return parsed command arguments"""
+        if self._args is None:
+            self._args = self._parser.parse_args()
+
+            for evt in self._events:
+                handler, args, kwargs = evt
+                handler(*args, **kwargs)
+
+        return self._args
+
+
+    def add_argument(self, *args, **kwargs):
+        """Calls argparse.ArgumentParser.add_argument"""
+        self._parser.add_argument(*args, **kwargs)
 
 
     def add_input_object(self):
         """Add options -i/--input-object-file and -f/--input-format"""
-        self.parser.add_argument('-i', '--input-object-file',
-                                 required=True,
-                                 help='Path to anndata or loom file.')
-        self.parser.add_argument('-f', '--input-format',
-                                 choices=['loom', 'anndata', 'auto-detect'],
-                                 default='auto-detect',
-                                 help='Format for input object: loom/anndata/[auto-detect].')
-        self._events.append({'handler':self._detect_io_format,
-                             'argv':['input_format', 'input_object_file']})
+        self._parser.add_argument('-i', '--input-object-file',
+                                  required=True,
+                                  help='Path to anndata or loom file.')
+        self._parser.add_argument('-f', '--input-format',
+                                  choices=['loom', 'anndata', 'auto-detect'],
+                                  default='auto-detect',
+                                  help='Format for input object: loom/anndata/[auto-detect].')
+        self._events.append([self._detect_io_format,
+                             ['input_format', 'input_object_file'], {}])
 
 
     def add_output_object(self):
         """Add options -o/--output-object-file and -F/--output-format"""
-        self.parser.add_argument('-o', '--output-object-file',
-                                 required=True,
-                                 help='File name in which to store serialized python object.')
-        self.parser.add_argument('-F', '--output-format',
-                                 choices=['loom', 'anndata', 'auto-detect'],
-                                 default='auto-detect',
-                                 help='Format for output object: loom/anndata/[auto-detect].')
-        self._events.append({'handler':self._detect_io_format,
-                             'argv':['output_format', 'output_object_file']})
+        self._parser.add_argument('-o', '--output-object-file',
+                                  required=True,
+                                  help='File name in which to store serialized python object.')
+        self._parser.add_argument('-F', '--output-format',
+                                  choices=['loom', 'anndata', 'auto-detect'],
+                                  default='auto-detect',
+                                  help='Format for output object: loom/anndata/[auto-detect].')
+        self._events.append([self._detect_io_format,
+                             ['output_format', 'output_object_file'], {}])
 
 
-    def add_subset_parameters(self):
-        """Add options -s/--subset-names, -l/--low-thresholds and -j/--high-thresholds"""
-        self.parser.add_argument('-s', '--subset-names',
-                                 required=True,
-                                 type=comma_separated_list('subset-names', str),
-                                 default=[],
-                                 help='Parameters to subset on.')
-        self.parser.add_argument('-l', '--low-thresholds',
-                                 type=comma_separated_list('low-thresholds', float),
-                                 default=[],
-                                 help='Low cutoffs for the parameters (default is -Inf).')
-        self.parser.add_argument('-j', '--high-thresholds',
-                                 type=comma_separated_list('high-thresholds', float),
-                                 default=[],
-                                 help='High cutoffs for the parameters (default is Inf).')
-        self._events.append({'handler':self._check_parameter_range,
-                             'argv':['subset_names', 'low_thresholds', 'high_thresholds']})
+    def add_output_plot(self):
+        """Add options -P/--output-plot"""
+        self._parser.add_argument('-P', '--output-plot',
+                                  default=None,
+                                  help='Save plot in the specified file')
+        self._events.append([self._check_filename_extension,
+                             ['output_plot'],
+                             {'extensions': ['pdf', 'png', 'svg']}])
+
+
+    def add_subset_parameters(self, params=None):
+        """Add options -p/--parameter-names, -l/--low-thresholds and -j/--high-thresholds
+
+        * Parameters
+            + params : list
+            If given, only parameters in this list are permitted.
+        """
+        self._parser.add_argument('-p', '--parameter-names',
+                                  required=True,
+                                  type=comma_separated_list('parameter-names', str),
+                                  default=[],
+                                  help='Parameters to subset on.' +
+                                  (' Choose from {}'.format(params) if params else ''))
+        self._parser.add_argument('-l', '--low-thresholds',
+                                  type=comma_separated_list('low-thresholds', float),
+                                  default=[],
+                                  help='Low cutoffs for the parameters (default is -Inf).')
+        self._parser.add_argument('-j', '--high-thresholds',
+                                  type=comma_separated_list('high-thresholds', float),
+                                  default=[],
+                                  help='High cutoffs for the parameters (default is Inf).')
+        self._events.append([self._check_parameter_range,
+                             ['parameter_names', 'low_thresholds', 'high_thresholds'],
+                             {'params':params}])
+
+    def add_subset_list(self, dtype=str):
+        """Add option -s/--subset-list
+
+        * Parameters
+            + dtype : type or function
+            Python data type or function that convert a string to a value of specified type.
+        """
+        self._parser.add_argument('-s', '--subset-list',
+                                  default='',
+                                  help='Comma-separated list of entries to use as a subset. '
+                                  'Alternatively, text file with one entry per line.')
+        self._events.append([self._list_or_read_file,
+                             ['subset_list'],
+                             {'dtype':dtype}])
 
 
     def _set_logging_level(self):
@@ -127,42 +183,72 @@ class ScanpyArgParser():
             setattr(self._args, fmt_key, 'anndata')
         else:
             msg = ('Unspecified unknown format: "{}", '
-                   'please check suffix is either ".loom" or ".h5ad"').format(fname)
+                   'please check extension is either ".loom" or ".h5ad"').format(fname)
             logging.error(msg)
             sys.exit(1)
 
 
-    def _check_parameter_range(self, name_key, low_key, high_key):
-        name_arg, low_arg, high_arg = [k.replace('_', '-') for k in (name_key, low_key, high_key)]
+    def _check_filename_extension(self, fname_key, extensions):
+        fname_opt = fname_key.replace('_', '-')
+        fname = getattr(self._args, fname_key)
+        if not fname:
+            return
+        ext = os.path.splitext(fname)[1]
+        if extensions and ext[1:] not in extensions:
+            msg = ('Unsupported file format from --{}: {}. '
+                   'Choose from {}').format(fname_opt, fname, extensions)
+            logging.error(msg)
+            sys.exit(1)
+        setattr(self._args, fname_key + '_format', ext[1:])
+
+
+    def _check_parameter_range(self, name_key, low_key, high_key, params=None):
+        name_opt, low_opt, high_opt = [k.replace('_', '-') for k in (name_key, low_key, high_key)]
         names, lows, highs = [getattr(self._args, k) for k in (name_key, low_key, high_key)]
+        if not names:
+            return
+        if params:
+            for name in names:
+                if name not in params:
+                    msg = 'Unsupported subset parameter: {}. Choose from {}'.format(name, params)
+                    logging.error(msg)
+                    sys.exit(1)
         n_par = len(names)
         if not lows:
             lows = [float('-Inf')] * n_par
         elif len(lows) != n_par:
             msg = ('--{} should be a comma separated list of the same size as --{}').format(
-                low_arg, name_arg)
+                low_opt, name_opt)
             logging.error(msg)
             sys.exit(1)
         if not highs:
             highs = [float('Inf')] * n_par
         elif len(highs) != n_par:
             msg = ('--{} should be a comma separated list of the same size as --{}').format(
-                high_arg, name_arg)
+                high_opt, name_opt)
             logging.error(msg)
             sys.exit(1)
 
 
-    def get_args(self):
-        """Return parsed command arguments"""
-        if self._args is None:
-            self._args = self.parser.parse_args()
-
-            for evt in self._events:
-                handler = evt['handler']
-                argv = evt['argv']
-                handler(*argv)
-
-        return self._args
+    def _list_or_read_file(self, key, dtype=str):
+        opt = key.replace('_', '-')
+        str_list_parser = comma_separated_list(opt, dtype)
+        input_string = getattr(self._args, key)
+        if input_string:
+            if ',' in input_string:
+                entries = str_list_parser(input_string)
+            elif os.path.isfile(input_string):
+                entries = list(pd.read_table(input_string,
+                                             header=None).iloc[:, 0].values.astype(dtype))
+            else:
+                try:
+                    entries = [dtype(input_string)]
+                except ValueError:
+                    msg = '--{} expects comma separated list of [{}] but received "{}"'.format(
+                        opt, dtype.__name__, input_string)
+                    logging.error(msg)
+                    sys.exit(1)
+            setattr(self._args, key, entries)
 
 
 def read_input_object(filename, fmt):
@@ -205,3 +291,22 @@ def write_output_object(adata, filename, fmt):
     else:
         logging.error('should not reach here')
         sys.exit(1)
+
+
+def save_output_plot(func_name, filename):
+    """Save output plot to the specified location
+
+    * Parameters
+        + func_name : str
+        name of the function that produced the plot
+        + filename: str
+        path of the saved plot
+    """
+    autosaved = '{}{}{}.{}'.format(sc.settings.figdir,
+                                   func_name,
+                                   sc.settings.plot_suffix,
+                                   sc.settings.file_format_figs)
+    dirname = os.path.split(filename)[0]
+    if dirname and not os.path.exists(dirname):
+        os.makedirs(dirname)
+    shutil.move(autosaved, filename)
