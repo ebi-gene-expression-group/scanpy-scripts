f93d9d4690fbeaf6649ef63eea994e018e07f5fe
nh3 <nh3@users.noreply.github.com>
2018-10-15 16:27:46 +0100

    Allow to save raw quantification before normalisation

--- bin/scanpy-normalise-data.py
+++ bin/scanpy-normalise-data.py
@@ -11,6 +11,9 @@ def main(args):
 
     adata = read_input_object(args.input_object_file, args.input_format)
 
+    if args.save_raw:
+        adata.raw = sc.pp.log1p(adata, copy=True)
+
     sc.pp.normalize_per_cell(adata, counts_per_cell_after=args.scale_factor)
 
     write_output_object(adata, args.output_object_file, args.output_format)
@@ -27,6 +30,10 @@ if __name__ == '__main__':
                            type=float,
                            default=1e4,
                            help='Aimed counts per cell after normalisation, default: 1e4')
+    argparser.add_argument('-r', '--save-raw',
+                           action='store_true',
+                           default=False,
+                           help='Save raw quantification in log scale before normalisation.')
     args = argparser.get_args()
 
     main(args)
