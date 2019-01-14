ee40b384ebfe724bb347d6eac9818d8f2273d346
nh3 <nh3@users.noreply.github.com>
2019-01-14 13:19:52 +0000

    v0.2.x (#29)
    * Replace non-ascii left and right single quote with double quotes
    
    * Create .gitattributes
    
    Ignore .travis.yml during `git archive`
    
    * Fix scanpy version requirement to v1.3.2
    
    * Fix bug in handling provided subset in filter-cells
    
    * Explicitly check number of genes passing filter instead of using assertion
    
    * Rename License.txt to LICENSE to be compatible with pypi requirements
    
    * Add content of README.md as long description to setup.py, change version to 0.1.0.2
    
    * Update gitignore
    
    * Re-implementing scanpy read() and filter() based on click.

--- setup.py
+++ setup.py
@@ -12,12 +12,17 @@ scripts = [
     for name in script_names
 ]
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
 setup(
     name='scanpy-scripts',
-    version='1.0',
-    description='Scripts for using scanpy from the command line',
+    version='0.1.0.2',
     author='nh3',
-    #author_email='',
+    author_email='',
+    description='Scripts for using scanpy from the command line',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/ebi-gene-expression-group/scanpy-scripts',
     packages=find_namespace_packages(),
     scripts=[
