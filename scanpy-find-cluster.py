2f9150c1214e844e2e661f652191ce77c6193a05
nh3 <nh3@users.noreply.github.com>
2019-02-20 15:00:54 +0000

    Support taking a list of resolution values and run clustering multiple times

--- scanpy-find-cluster.py
+++ scanpy-find-cluster.py
@@ -14,18 +14,25 @@ def main(args):
 
     if args.restrict_to is not None:
         args.restrict_to = (args.restrict_to[0], args.restrict_to[1:])
-    sc.tl.louvain(adata,
-                  flavor=args.flavor,
-                  resolution=args.resolution,
-                  restrict_to=args.restrict_to,
-                  key_added=args.key_added,
-                  use_weights=args.use_weights,
-                  random_state=args.random_seed)
+    cluster_columns = []
+    for res in args.resolution:
+        if len(args.resolution) == 1:
+            key = args.key_added
+        else:
+            key = '{}_r{}'.format(args.key_added, res)
+        sc.tl.louvain(adata,
+                      flavor=args.flavor,
+                      resolution=res,
+                      restrict_to=args.restrict_to,
+                      key_added=key,
+                      use_weights=args.use_weights,
+                      random_state=args.random_seed)
+        cluster_columns.append(key)
 
     write_output_object(adata, args.output_object_file, args.output_format)
 
     if args.output_text_file:
-        adata.obs[[args.key_added]].reset_index(level=0).rename(columns={'index':'cells'}).to_csv(
+        adata.obs[cluster_columns].reset_index(level=0).rename(columns={'index':'cells'}).to_csv(
             args.output_text_file, sep='\t', index=None)
 
     print(adata)
@@ -46,11 +53,12 @@ if __name__ == '__main__':
                            help='Choose between two packages for computing the clustering.'
                                 '"vtraag" is much more powerful, and the default.')
     argparser.add_argument('--resolution',
-                           type=float,
-                           default=1.0,
+                           type=comma_separated_list('resolution', float),
+                           default=[1.0],
                            help='For the default flavor "vtraag", you can provide a resolution '
                                 '(higher resolution means finding more and smaller clusters). '
-                                'Default: 1.0')
+                                'Multiple resolutions can be provided as a comma-separated-list, '
+                                'e.g. "0.5,1.0,1.5". Default: 1.0')
     argparser.add_argument('--restrict-to',
                            type=comma_separated_list('restrict-to', str),
                            default=None,
@@ -59,7 +67,9 @@ if __name__ == '__main__':
                                 'categories).')
     argparser.add_argument('--key-added',
                            default='louvain',
-                           help='Key under which to add the cluster labels. Default: louvain')
+                           help='Key under which to add the cluster labels. If more than one '
+                           'resolution is provided, the key is used as prefix where the full key '
+                           'will be "{prefix}_r{resolution}". Default: louvain')
     argparser.add_argument('--use-weights',
                            action='store_true',
                            default=False,
