1febb68812228845cf1b04af8e6c35487ca2e548
Ni Huang <nh3@mib112657i.internal.sanger.ac.uk>
2019-04-17 17:29:50 +0100

    Expose useful functions and package info

---
+++ scanpy_scripts/__init__.py
@@ -0,0 +1,13 @@
+"""
+Provides version, author and exports
+"""
+
+__version__ = '0.2.0'
+
+__author__ = ', '.join([
+    'Ni Huang',
+    'Pablo Moreno',
+    'Jonathan Manning',
+])
+
+from . import lib
---
+++ scanpy_scripts/lib/__init__.py
@@ -0,0 +1,9 @@
+"""
+Provides exported functions
+"""
+
+from ._read import read_10x
+from ._filter import filter_anndata
+from ._norm import normalize
+from ._hvg import hvg
+from ._neighbors import neighbors
