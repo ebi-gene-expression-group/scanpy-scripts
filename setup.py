e31c38da43e25abfb383544e90a6c048bfadb000
nh3 <nh3@users.noreply.github.com>
2019-04-17 13:50:30 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,41 +1,32 @@
-from setuptools import setup, find_namespace_packages
-from pathlib import Path
-
-HERE = Path(__file__).parent
-
-script_names = [
-    path.with_suffix("").name
-    for path in (HERE / 'scanpy_scripts').glob('scanpy_*.py')
-]
-scripts = [
-    f'{name.replace("_", "-")}=scanpy_scripts.{name}:main'
-    for name in script_names
-]
+from setuptools import setup, find_packages
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setup(
     name='scanpy-scripts',
-    version='0.1.0.2',
+    version='0.2.0',
     author='nh3',
-    author_email='',
+    author_email='nh3@users.noreply.github.com',
     description='Scripts for using scanpy from the command line',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/ebi-gene-expression-group/scanpy-scripts',
-    packages=find_namespace_packages(),
+    packages=find_packages(),
     scripts=[
         'scanpy-scripts-tests.sh',
         'scanpy-scripts-tests.bats',
     ],
     entry_points=dict(
-        console_scripts=scripts,
+        console_scripts='scanpy=scanpy_scripts.cli:cli',
     ),
     install_requires=[
         'matplotlib',
         'pandas',
-        'scanpy==1.3.2',
-        'louvain'
+        'scanpy>=1.4.0',
+        'louvain',
+        'leidenalg',
+        'MulticoreTSNE',
+        'Click'
     ],
 )
