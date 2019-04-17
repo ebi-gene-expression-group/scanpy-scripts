910f9b6ddbd4636ee0f79f0d9d307a98416dadd1
nh3 <nh3@users.noreply.github.com>
2019-04-17 14:03:03 +0100

    new interface using click

---
+++ scanpy_scripts/cli.py
@@ -0,0 +1,60 @@
+"""
+scanpy
+"""
+
+import logging
+import click
+import scanpy as sc
+from .cmds import (
+    READ_CMD,
+    FILTER_CMD,
+    NORM_CMD,
+    HVG_CMD,
+    SCALE_CMD,
+    REGRESS_CMD,
+    PCA_CMD,
+    NEIGHBOR_CMD,
+)
+
+
+@click.group()
+@click.option(
+    '--debug',
+    is_flag=True,
+    default=False,
+    help='Print debug information',
+)
+@click.option(
+    '--verbosity',
+    type=click.INT,
+    default=3,
+    help='Set scanpy verbosity',
+)
+@click.version_option(
+    version='0.2.0',
+    prog_name='scanpy',
+)
+def cli(debug=False, verbosity=3):
+    """
+    Command line interface to [scanpy](https://github.com/theislab/scanpy)
+    """
+    log_level = logging.DEBUG if debug else logging.INFO
+    logging.basicConfig(
+        level=log_level,
+        format=('%(asctime)s; %(levelname)s; %(filename)s; '
+                '%(funcName)s(): %(message)s'),
+        datefmt='%y-%m-%d %H:%M:%S',
+    )
+    logging.debug('debugging')
+    sc.settings.verbosity = verbosity
+    return 0
+
+
+cli.add_command(READ_CMD)
+cli.add_command(FILTER_CMD)
+cli.add_command(NORM_CMD)
+cli.add_command(HVG_CMD)
+cli.add_command(SCALE_CMD)
+cli.add_command(REGRESS_CMD)
+cli.add_command(PCA_CMD)
+cli.add_command(NEIGHBOR_CMD)
---
+++ scanpy_scripts/click_utils.py
@@ -0,0 +1,81 @@
+"""
+Provide helper functions for command line parsing with click
+"""
+
+import click
+
+
+class CommaSeparatedText(click.ParamType):
+    """
+    Comma separated text
+    """
+    def __init__(self, dtype=click.STRING, simplify=False):
+        self.dtype = dtype
+        self.dtype_name = _get_type_name(dtype)
+        self.name = '{}[,{}...]'.format(self.dtype_name, self.dtype_name)
+        self.simplify = simplify
+
+    def convert(self, value, param, ctx):
+        try:
+            converted = list(map(self.dtype, value.split(',')))
+            if self.simplify and len(converted) == 1:
+                converted = converted[0]
+            return converted
+        except ValueError:
+            self.fail('{} is not a valid comma separated list of {}'.format(
+                value, self.dtype_name), param, ctx)
+
+
+def _get_type_name(obj):
+    name = 'text'
+    try:
+        name = getattr(obj, 'name')
+    except AttributeError:
+        name = getattr(obj, '__name__')
+    return name
+
+
+def valid_limit(ctx, param, value):
+    if value[0] > value[1]:
+        param.type.fail(
+            'lower limit must not exceed upper limit', param, ctx)
+    return value
+
+
+def valid_parameter_limits(ctx, param, value):
+    for val in value:
+        if val[1] > val[2]:
+            param.type.fail(
+                'lower limit must not exceed upper limit', param, ctx)
+    return value
+
+
+def mutually_exclusive_with(param_name):
+    internal_name = param_name.strip('-').replace('-', '_').lower()
+    def valid_mutually_exclusive(ctx, param, value):
+        try:
+            other_value = ctx.params[internal_name]
+        except KeyError:
+            return value
+        if (value is None) == (other_value is None):
+            param.type.fail(
+                'mutually exclusive with "{}", one and only one must be '
+                'specified.'.format(param_name),
+                param,
+                ctx,
+            )
+        return value
+    return valid_mutually_exclusive
+
+
+def required_by(param_name):
+    internal_name = param_name.strip('-').replace('-', '_').lower()
+    def required(ctx, param, value):
+        try:
+            other_value = ctx.params[internal_name]
+        except KeyError:
+            return value
+        if other_value is not None and value is None:
+            param.type.fail('required by "{}".'.format(param_name), param, ctx,)
+        return value
+    return required
