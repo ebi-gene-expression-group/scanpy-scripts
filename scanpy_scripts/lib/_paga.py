692d6d2defd45d69f1738f955eb4beead75c1149
nh3 <nh3@users.noreply.github.com>
2019-06-19 13:03:16 +0100

    Improve handling of slots and bugfix (#42)
    * Merge bugfixes and improvements from v0.2.x_feature/nh3
    
    * Update lib/__init__.py
    
    * Add inline documentation for slot/key handling functions

--- scanpy_scripts/lib/_paga.py
+++ scanpy_scripts/lib/_paga.py
@@ -4,9 +4,11 @@ scanpy paga
 
 import scanpy as sc
 from ..cmd_utils import (
+    _backup_default_key,
+    _delete_backup_key,
+    _rename_default_key,
     _set_default_key,
     _restore_default_key,
-    _rename_default_key,
 )
 
 
@@ -19,12 +21,17 @@ def paga(
     """
     Wrapper function for sc.tl.paga, for supporting named slot
     """
-    _set_default_key(adata, 'uns', 'neighbors', use_graph)
+    _set_default_key(adata.uns, 'neighbors', use_graph)
+    _backup_default_key(adata.uns, 'paga')
+
     sc.tl.paga(adata, **kwargs)
-    _restore_default_key(adata, 'uns', 'neighbors', use_graph)
+
+    _restore_default_key(adata.uns, 'neighbors', use_graph)
 
     if key_added:
         paga_key = f'paga_{key_added}'
-        _rename_default_key(adata, 'uns', 'paga', paga_key)
+        _rename_default_key(adata.uns, 'paga', paga_key)
+    else:
+        _delete_backup_key(adata.uns, 'paga')
 
     return adata
