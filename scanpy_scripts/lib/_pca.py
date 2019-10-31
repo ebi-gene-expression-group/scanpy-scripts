98cb74731be693b8709c0439e070bcc7a2f6bbb9
nh3 <nh3@users.noreply.github.com>
2019-10-31 09:53:45 +0000

    Add option remove_cc in pca()

--- scanpy_scripts/lib/_pca.py
+++ scanpy_scripts/lib/_pca.py
@@ -5,10 +5,18 @@ scanpy pca
 import scanpy as sc
 from ..obj_utils import write_embedding
 
-def pca(adata, key_added=None, export_embedding=None, **kwargs):
+def pca(adata, key_added=None, remove_cc=False, export_embedding=None, **kwargs):
     """
     Wrapper function for sc.pp.pca, for supporting named slot
     """
+    remove_cc_from_hvg = (
+        remove_cc and 'cc' in adata.var.columns
+        and (adata.var['cc'].dtype.kind == 'b' or isinstance(adata.var['cc'][0], bool))
+    )
+    if remove_cc_from_hvg:
+        adata.var['hvg_full'] = adata.var['highly_variable'].copy()
+        adata.var['highly_variable'] = adata.var['highly_variable'] & ~adata.var['cc']
+
     # omit "svd_solver" to let scanpy choose automatically
     if 'svd_solver' in kwargs and kwargs['svd_solver'] == 'auto':
         del kwargs['svd_solver']
@@ -26,6 +34,10 @@ def pca(adata, key_added=None, export_embedding=None, **kwargs):
     else:
         sc.pp.pca(adata, **kwargs)
         pca_key = 'X_pca'
+
+    if remove_cc_from_hvg:
+        adata.var['highly_variable'] = adata.var['hvg_full']
+        del adata.var['hvg_full']
 
     if export_embedding is not None:
         write_embedding(adata, pca_key, export_embedding, key_added=key_added)
