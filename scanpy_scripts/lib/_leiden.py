c62bba36ad9f053d966cf0d859594fa45200096b
nh3 <nh3@users.noreply.github.com>
2020-01-08 11:25:54 +0000

    Really fix `restrict_to` in louvain() and leiden() again

--- scanpy_scripts/lib/_leiden.py
+++ scanpy_scripts/lib/_leiden.py
@@ -21,7 +21,7 @@ def leiden(
     if ('restrict_to' in kwargs
             and not (isinstance(kwargs['restrict_to'], (list, tuple))
                 and len(kwargs['restrict_to']) == 2
-                and kwargs['restrict_to'])):
+                and kwargs['restrict_to'][0])):
         del kwargs['restrict_to']
     adj_mat = None
     if use_graph:
