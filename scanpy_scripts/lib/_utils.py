7c409052c8d64ac9cf3ebc95d8cf8fec69ddf499
nh3 <nh3@users.noreply.github.com>
2020-03-17 10:48:51 +0000

    Major update of utility functions

--- scanpy_scripts/lib/_utils.py
+++ scanpy_scripts/lib/_utils.py
@@ -3,6 +3,7 @@ Utility functions
 """
 
 import numpy as np
+import scipy.sparse as sp
 import pandas as pd
 import scanpy as sc
 
@@ -75,20 +76,48 @@ def run_bbknn(adata, batch, use_rep='X_pca', key_added='bk', **kwargs):
             del adata.obsm['X_pca_bkup']
 
 
-def run_seurat_integration(
-        adata,
-):
-    pass
+def run_phate(adata, use_rep='X', key_added=None, knn=5, decay=40, t='auto', n_pca=100, random_state=0, **kwargs):
+    import phate
+    if use_rep == 'X':
+        data = adata.X
+    elif use_rep == 'raw':
+        data = adata.raw.X
+    elif use_rep in adaat.obsm.keys():
+        data = adata.obsm[use_rep]
+    else:
+        raise KeyError(f'{use_rep} not found.')
+    phate_operator = phate.PHATE(
+        knn=knn, decay=decay, t=t, n_pca=n_pca, random_state=random_state, **kwargs)
+    kc_phate = phate_operator.fit_transform(data)
+    slot_name = f'X_phate_{key_added}' if key_added else 'X_phate'
+    ad_kc.obsm[slot_name] = kc_phate
 
 
-def split_by_group(adata, groupby):
+def split_by_group(adata, groupby, out_type='dict'):
     if groupby not in adata.obs.columns:
         raise KeyError(f'{groupby} is not a valid obs annotation.')
-    groups = adata.obs[groupby].unique().to_list()
-    adata_dict = {}
-    for grp in groups:
-        adata_dict[grp] = adata[adata.obs[groupby] == grp, :]
-    return adata_dict
+    groups = sorted(list(adata.obs[groupby].unique()))
+    if out_type == 'dict':
+        out_adatas = {}
+        for grp in groups:
+            out_adatas[grp] = adata[adata.obs[groupby] == grp, :].copy()
+    elif out_type == 'list':
+        out_adatas = []
+        for grp in groups:
+            out_adatas.append(adata[adata.obs[groupby] == grp, :].copy())
+    else:
+        raise ValueError(f'{out_type}: unsupported type, choose from "dict" or "list".')
+    return out_adatas
+
+
+def show_obs_categories(ad, columns=None):
+    if columns:
+        columns = [k for k in columns if k in ad.obs.columns]
+    else:
+        columns = [k for k in ad.obs.columns if ad.obs[k].dtype.name == 'category']
+    for k in columns:
+        print(k)
+        print(ad.obs[k].value_counts().to_dict())
 
 
 def regroup(adata, groupby, regroups):
@@ -102,7 +131,11 @@ def regroup(adata, groupby, regroups):
                 new_groups[groups == grp] = new_grp
         else:
             new_groups[groups == old_grps] = new_grp
-    return new_groups.astype('category')
+    regroup_keys = [g for g in regroups.keys() if g in set(new_groups.unique())]
+    new_groups = new_groups.astype('category')
+    if len(regroup_keys) == len(new_groups.cat.categories):
+        new_groups = new_groups.cat.reorder_categories(regroup_keys)
+    return new_groups
 
 
 def subsample(adata, fraction, groupby=None, min_n=0, max_n=10000, **kwargs):
@@ -142,12 +175,21 @@ def pseudo_bulk(
     n_level = len(groups)
     if highly_variable:
         if isinstance(highly_variable, (list, tuple)):
-            k_hv = adata.var_names.isin(highly_variable)
+            if use_rep == 'raw':
+                k_hv = adata.raw.var_names.isin(highly_variable)
+            else:
+                k_hv = adata.var_names.isin(highly_variable)
         else:
             k_hv = adata.var['highly_variable'].values
     if use_rep == 'X':
         x = adata.X
         features = adata.var_names.values
+        if highly_variable:
+            x = x[:, k_hv]
+            features = features[k_hv]
+    elif use_rep == 'raw':
+        x = adata.raw.X
+        features = adata.raw.var_names.values
         if highly_variable:
             x = x[:, k_hv]
             features = features[k_hv]
@@ -169,83 +211,8 @@ def pseudo_bulk(
     summarised = np.zeros((n_level, x.shape[1]))
     for i, grp in enumerate(groups):
         k_grp = group_attr == grp
-        summarised[i] = FUN(x[k_grp, :], axis=0, keepdims=True)
-    return pd.DataFrame(summarised.T, columns=groups, index=features)
-
-
-def LR_annotate(
-        adata,
-        train_label,
-        train_x,
-        use_rep='X',
-        highly_variable=True,
-        subset=None,
-        penalty='l1',
-        sparcity=0.2,
-        return_label=False,
-):
-    """Annotate cells using logistic regression
-    """
-    if subset is not None:
-        train_label = train_label[subset]
-        train_x = train_x[subset, :]
-    from sklearn.linear_model import LogisticRegression
-    lr = LogisticRegression(penalty=penalty, C=sparcity)
-    lr.fit(train_x, train_label)
-    if use_rep == 'X':
-        if highly_variable:
-            predict_x = adata.X[:, adata.var.highly_variable]
+        if sp.issparse(x):
+            summarised[i] = FUN(x[k_grp, :], axis=0)
         else:
-            predict_x = adata.X
-    elif use_rep in adata.layers.keys():
-        predict_x = adata.layers[use_rep]
-    elif use_rep in adata.obsm.keys():
-        predict_x = adata.obsm[use_rep]
-    if return_label:
-        return lr.predict(predict_x)
-    else:
-        return lr.predict_proba(predict_x)
-
-
-def annotate(adata, groupby, annot, annotation_matrix):
-    """Annotate a clustering based on a matrix of values
-
-    + groupby: the clustering to be annotated
-    + annot : the annotationo to be aligned/transferred
-    + annotation_matrix: a matrix with shape (n,m) where n equals the number
-                         of clusters and m equals the number of annotation
-                         categories.
-    """
-    if groupby not in adata.obs.columns:
-        raise KeyError(f'"{groupby}" not found.')
-    if annot not in adata.obs.columns:
-        raise KeyError(f'"{annot}" not found.')
-    groupby_annot = f'{groupby}_annot'
-    clustering = adata.obs[groupby]
-    annotation = adata.obs[annot]
-    k_nan = annotation == 'nan'
-    cell_types = np.unique(annotation[~k_nan].values)
-    n_celltype = cell_types.shape[0]
-    n_cluster = np.unique(clustering).shape[0]
-    n_cell = clustering.shape[0]
-
-    def dedup(names):
-        names_copy = names.copy()
-        count = {}
-        for i, name in enumerate(names):
-            n = count.get(name, 0) + 1
-            count[name] = n
-            if n > 1 or name in names[(i+1):]:
-                names_copy[i] = f'{name}{n}'
-            else:
-                names_copy[i] = name
-        return names_copy
-
-    if (annotation_matrix.shape[0] == n_cluster and
-            annotation_matrix.shape[1] == n_celltype):
-        k_max = np.argmax(annotation_matrix, axis=1)
-        cluster_annot = cell_types[k_max]
-        adata.obs[groupby_annot] = adata.obs[groupby].cat.rename_categories(
-                dedup(cluster_annot))
-    else:
-        raise ValueError(f'[annotation_matrix] not in compatible shape')
+            summarised[i] = FUN(x[k_grp, :], axis=0, keepdims=True)
+    return pd.DataFrame(summarised.T, columns=groups, index=features)
