6a5205da2e87f6b8bfc2feb091d445bdf185ef0a
Jonathan Manning <jmanning@ebi.ac.uk>
2021-04-21 12:35:11 +0100

    Fix conditional

--- scanpy_scripts/lib/_norm.py
+++ scanpy_scripts/lib/_norm.py
@@ -13,7 +13,7 @@ def normalize(adata, save_raw='yes', save_raw_location='raw', log_transform=True
     if save_raw == 'counts':
         if save_raw_location == 'raw':
             adata.raw = adata
-        else
+        else:
             adata.layers[save_raw_location] = adata.X
     sc.pp.normalize_total(adata, **kwargs)
     if log_transform:
@@ -21,7 +21,7 @@ def normalize(adata, save_raw='yes', save_raw_location='raw', log_transform=True
     if save_raw == 'yes':
         if save_raw_location == 'raw':
             adata.raw = adata
-        else
+        else;
             adata.layers[save_raw_location] = adata.X
 
     return adata
