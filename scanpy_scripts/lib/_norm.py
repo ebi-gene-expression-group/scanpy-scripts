11f57014548260b3fa801e11d679d968c22db635
Pablo Moreno <pcm32@users.noreply.github.com>
2022-11-30 13:31:48 +0000

    Set base explicitly to avoid it being dropped

--- scanpy_scripts/lib/_norm.py
+++ scanpy_scripts/lib/_norm.py
@@ -3,6 +3,7 @@ scanpy norm
 """
 
 import scanpy as sc
+import math
 
 
 def normalize(adata, log_transform=True, **kwargs):
@@ -12,10 +13,15 @@ def normalize(adata, log_transform=True, **kwargs):
     """
     sc.pp.normalize_total(adata, **kwargs)
     if log_transform:
-        sc.pp.log1p(adata)
+        # Natural logarithm is the default by scanpy, if base is not set
+        base = math.e
+        sc.pp.log1p(adata, base=base)
+        # scanpy is not setting base in uns['log1p'] keys, but later on asking for it
+        if "log1p" in adata.uns_keys() and "base" not in adata.uns["log1p"]:
+            # Note that setting base to None doesn't solve the problem at other modules that check for base later on
+            # as adata.uns["log1p"]["base"] = None gets dropped at either anndata write or read.
+            adata.uns["log1p"]["base"] = base
 
-    # scanpy is not setting base in uns['log1p'] keys, but later on asking for it
-    if "log1p" in adata.uns_keys() and "base" not in adata.uns["log1p"]:
-        adata.uns["log1p"]["base"] = None
+    
 
     return adata
